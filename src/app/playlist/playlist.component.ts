c5c7fcdce35f4beccc57e1d1d14f037265325cdd
thedub2001 <thedub2001@hotmail.com>
2021-08-04 22:16:19 +0200

    playlistDetail design

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,8 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { MixcloudService } from '../mixcloud.service';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Location }                 from '@angular/common';
 
+export interface playlistDetailData {
+  tracks: [
+    {
+      audio_length:number,
+      key:string,
+      name:string
+    }
+  ];
+}
+
+// TO DO : ajouter icone à la place de "play"
+// TO DO : playTrack();
 
 @Component({
   selector: 'app-playlist',
@@ -11,7 +23,10 @@ import { Location }                 from '@angular/common';
 })
 export class PlaylistComponent implements OnInit {
   playlistName : string;
-  playlistDetail=[];
+  playlistDetail : playlistDetailData ;
+  dataSource = [];
+  displayedColumns: string[] = ['name', 'time', 'action'];
+
 
   constructor(
     public mix : MixcloudService,
@@ -22,7 +37,35 @@ export class PlaylistComponent implements OnInit {
   ngOnInit(): void {
     this.playlistName = this.route.snapshot.paramMap.get('id');
     this.playlistDetail=this.mix.mixcloudPlaylistsContent.find(p=>p.name===this.playlistName)
+    this.dataSource = this.playlistDetail.tracks.map(obj => {
+      var rObj = {};
+      rObj["name"] = obj.name;
+      rObj["key"] = obj.key;
+      rObj["audio_length"] = this.secondsToHms(obj.audio_length);
+      return rObj;
+    });
   }
 
+
+  playTrack(): void {
+    console.log("play track from playlistDetail")
+
+  }
+
+   secondsToHms(d:number) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = this.checkTime(h);
+    var mDisplay = this.checkTime(m);
+    var sDisplay = this.checkTime(s);
+    return hDisplay+":"+mDisplay+":"+sDisplay; 
+}
+
+ checkTime(i:number) {
+  return (i < 10) ? "0" + i : i;
+}
 
 }
