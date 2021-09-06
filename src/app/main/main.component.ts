205c022ebc0e23a833c76b8ecd496a484b5570a7
thedub2001 <thedub2001@hotmail.com>
2021-09-06 21:21:39 +0200

    theming and animation features

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,102 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild,HostListener  } from '@angular/core';
 import { AudioPlayerComponent , Track} from 'ngx-audio-player';
 import { SoundcloudService } from '../soundcloud.service';
 import { Router } from '@angular/router';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import {trigger,state,style,animate,transition} from "@angular/animations";
+
+// TO DO : export animations in a separate file
+// TO DO : replace first logo by vinylLogo not spinning
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations: [
+    trigger("vinylAnim", [
+      state(
+        "initial",
+        style({
+          zIndex: "3",
+          position : "relative",
+
+        })
+      ),
+      state(
+        "0",
+        style({
+          transform: "rotate(360deg)",
+          zIndex: "3",
+          position : "relative",
+
+
+        })
+      ),
+      state(
+        "1",
+        style({
+          transform: "rotate(540deg)",
+          zIndex: "3",
+          position : "relative",
+
+        })
+      ),
+      state(
+        "2",
+        style({
+          transform: "rotate(720deg)",
+          zIndex: "3",
+          position : "relative",
+          display :"none",
+          opacity : "0",
+
+        })
+      ),
+      transition("initial=>0", animate("3.5s cubic-bezier(0.5,0,1,1)")),
+      transition("0=>1", animate("900ms")),
+      transition("1=>2", animate("900ms")),
+    ]),
+    
+    trigger("overlayFadeOut", [
+      state(
+        "hidden",
+        style({
+          position: "fixed",
+          display: "block",
+          width: "100%",
+          height: "100%",
+          top: "0",
+          left: "0",
+          right: "0",
+          bottom: "0",
+          backgroundColor: "#eeeeee",
+          zIndex: "2",
+          cursor: "pointer",
+        })
+      ),
+      state(
+        "visible",
+        style({
+          position: "fixed",
+          display: "none",
+          width: "100%",
+          height: "100%",
+          top: "0",
+          left: "0",
+          right: "0",
+          bottom: "0",
+          backgroundColor: "#eeeeee",
+          opacity : "0",
+          zIndex: "2",
+          cursor: "pointer",
+
+        })
+      ),
+      transition("hidden=>visible", animate("0.9s 5s ease-out")),
+    ])
+    
+  ]
 })
 
 export class MainComponent implements AfterViewInit, OnInit  {
@@ -17,6 +107,10 @@ export class MainComponent implements AfterViewInit, OnInit  {
   // See : https://www.pluralsight.com/guides/querying-the-dom-with-@viewchild-and-@viewchildren
   @ViewChild(AudioPlayerComponent, { static: false }) ngxPlayer : AudioPlayerComponent;
 
+  isHandset$= this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches)
+  );
   
   detailActivated: boolean = false;
 
@@ -38,9 +132,15 @@ export class MainComponent implements AfterViewInit, OnInit  {
   msaapDisplayDuration = false;
   msaapDisablePositionSlider = false;
   currentIndex: number;
-  tabIndex : number =0;
+  tabIndex : number = 0;
+  currentState:string = "initial";
+  overlayState: string = "hidden";
 
-  constructor(public sc : SoundcloudService,public router : Router) {}
+  constructor(
+    public sc : SoundcloudService,
+    public router : Router,
+    private breakpointObserver: BreakpointObserver,
+    ) {}
 
   ngOnInit(): void {
   }
@@ -105,5 +205,17 @@ export class MainComponent implements AfterViewInit, OnInit  {
     this.tabIndex=n;
   }
 
+  captureDoneEvent(e) : void {
+    // console.log("captureDoneEvent : ",e)
+    switch (e.toState) {
+      case 'initial' : this.currentState='0';
+      break;
+      case '0' : this.currentState='1';
+      break;
+      case '1' : this.currentState='2';
+      break;
+    }
+  }
+  
  }
 
