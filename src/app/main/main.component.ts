9f6ed742a6142697d49fcee73f28cb2891c53a72
thedub2001 <thedub2001@hotmail.com>
2021-12-10 19:29:09 +0100

    cleaned project from ngx and souncloud service features

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,8 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild,HostListener  } from '@angular/core';
-import { AudioPlayerComponent , Track} from 'ngx-audio-player';
-// import { SoundcloudService } from '../soundcloud.service';
 import { Router } from '@angular/router';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
-import {vinylAnimation, fadeoutAnimation} from "./animation";
+import { vinylAnimation, fadeoutAnimation } from "./animation";
 
 @Component({
   selector: 'app-main',
@@ -17,10 +15,6 @@ export class MainComponent implements AfterViewInit, OnInit  {
   @ViewChild('jsconsole') jsconsole: ElementRef;
   @ViewChild('playlist') playlist: ElementRef;
   
-  // we need to get the ElementRef of the HTML5 audio player wich is part of the AudioPlayerComponent
-  // See : https://www.pluralsight.com/guides/querying-the-dom-with-@viewchild-and-@viewchildren
-  @ViewChild(AudioPlayerComponent, { static: false }) ngxPlayer : AudioPlayerComponent;
-
   isHandset$= this.breakpointObserver.observe(Breakpoints.Handset)
   .pipe(
     map(result => result.matches)
@@ -31,21 +25,11 @@ export class MainComponent implements AfterViewInit, OnInit  {
   public play : boolean = false;
   public loaded : boolean = false;
   public pause : boolean = false;
-  public currentTrack : Track = {title:"",link:""};
   isOpen=false;
   sliderValue=5;
   public tempTest=0;
   sc = {};
 
-
-  msaapDisplayTitle = false;
-  msaapDisplayPlayList = false;
-  msaapPageSizeOptions = [2,4,6];
-  msaapDisplayVolumeControls = true;
-  msaapDisplayRepeatControls = true;
-  msaapDisplayArtist = false;
-  msaapDisplayDuration = false;
-  msaapDisablePositionSlider = false;
   currentIndex: number;
   tabIndex : number = 0;
   currentState:string = "initial";
@@ -69,55 +53,9 @@ export class MainComponent implements AfterViewInit, OnInit  {
   ngOnInit(): void {
   }
 
-  ngxAudioPlayerPlay(): void {
-    this.ngxPlayer.playBtnHandler();
-  }
-
-  ngxAudioPlayerPrevious(): void {
-    this.ngxPlayer.previousSong();
-  }
-
-  ngxAudioPlayerNext(): void {
-    this.ngxPlayer.nextSong();
-  }
-
   ngAfterViewInit(): void {
   }
-
-  onEnded(e) : void {
-    console.log("onEnded :",e);
-  }
   
-  onPlayed(e) : void {
-    console.log("onPlayed :",e);
-    this.play=true;
-    this.pause = false;
-
-  }
-  
-  onPaused(e) : void {
-    console.log("onPaused :",e);
-    this.play=false;
-    this.pause = true;
-  }
-    
-  onLoaded(e) : void {
-    console.log("onLoaded :",e);
-    this.loaded=true;
-    this.play=false;
-    this.pause = true;   
-    this.currentTrack=this.ngxPlayer.tracks[this.ngxPlayer.currentIndex];
-    this.currentIndex=this.ngxPlayer.currentIndex;
-
-  }
-
-  onLoading(e) : void {
-    console.log("onLoading :",e);
-    this.play=false;
-    this.loaded=false;
-
-  }
-
   navigateToPlaylist(p) : void {
     console.log(p);
     this.router.navigate(['./playlist',p]);
--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SoundcloudService } from '../soundcloud.service';
 import { ActivatedRoute } from '@angular/router';
-import { MainComponent } from '../main/main.component';
 
 @Component({
   selector: 'app-playlist',
@@ -18,40 +17,11 @@ export class PlaylistComponent implements OnInit {
   constructor(
     public sc : SoundcloudService,
     private route: ActivatedRoute ,
-    public main : MainComponent
   ) {}
 
   ngOnInit(): void {
     this.playlistName = this.route.snapshot.paramMap.get('id');
     this.dataSource = this.sc.getSoundcloudPlaylisTracks(this.playlistName);
   }
-  
-  playPlaylist(): void {
-    this.main.ngxPlayer.player.nativeElement.pause();
-    setTimeout(() => {
-      console.log("Playing all tracks from playlist ", this.playlistName )
-      this.sc.dataSource=this.dataSource;
-      //this.ngxPlayer is not working so we must use the one of the MainComponent
-      this.main.ngxPlayer.currentIndex =0;
-      setTimeout(() => {
-        this.main.ngxPlayer.player.nativeElement.play();
-      }, 50);
-    }, 50);
-
-
-  }
-  playTrack(m): void {
-    this.main.ngxPlayer.player.nativeElement.pause();
-    setTimeout(() => {
-      console.log("Playing one track from playlist with indexOfelement :", m )
-      this.sc.dataSource=[this.dataSource[m]];
-      //this.ngxPlayer is not working so we must use the one of the MainComponent
-      this.main.ngxPlayer.currentIndex =0;
-      setTimeout(() => {
-        this.main.ngxPlayer.player.nativeElement.play();
-      }, 50);
-    }, 50);
-  }
-
-  
+    
 }
